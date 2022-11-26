11bb4a650f4e69e2d167ac79d852e58edd615c4f
Ghaleon <harakirighaleon@gmail.com>
2022-11-27 02:06:15 +0300

    dobavilVvod

--- Program.cs
+++ Program.cs
@@ -2,16 +2,23 @@
 // Первоначальный массив можно ввести с клавиатуры, или задать на старте выполнения алгоритма. 
 // При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
-string[] only3Symbols(string[] array)
+string[] only3Symbols(string[] userArr)
 {   
     string stringResult = String.Empty;
-    for (int i = 0; i < array.Length; i++)
+    for (int i = 0; i < userArr.Length; i++)
     {
-        if (array[i].Length <= 3)
+        if (userArr[i].Length <= 3)
         {
-            stringResult = stringResult + array[i] + " ";
+            stringResult = stringResult + userArr[i] + ",";
         }
     }
-    string[] finalArr = stringResult.Split(" ");
+    string[] finalArr = stringResult.Split(",");
     return finalArr;
 }
+
+Console.Clear();
+Console.WriteLine("Задайте элементы массива разной длины через запятую: ");
+string userArr = (Console.ReadLine()!);
+string[] finalArr = userArr.Split(",");
+
+Console.WriteLine($"Был массив такой: [{userArr}], а стал такой: [{string.Join(",", only3Symbols(finalArr))}]");
