54c86b7bf02d10111fa0437a30d90c7fdb27c15d
Ghaleon <harakirighaleon@gmail.com>
2022-11-27 01:32:48 +0300

    vvedenMetod

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует массив из строк, чья длина не превышает 3 символа. 
+// Первоначальный массив можно ввести с клавиатуры, или задать на старте выполнения алгоритма. 
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+string[] only3Symbols(string[] array)
+{   
+    string stringResult = String.Empty;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            stringResult = stringResult + array[i] + " ";
+        }
+    }
+    string[] finalArr = stringResult.Split(" ");
+    return finalArr;
+}
